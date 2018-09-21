ad2219a8ac94dea17ee44c82729d7ba347ced52b
Jonathan <jalmeida0291@gmail.com>
2018-09-20 20:13:37 -0400

    game logic is now implemented

--- app.js
+++ app.js
@@ -1,13 +1,36 @@
+// Global variables
+
 const container = document.querySelector(".container");
 const cards = document.querySelectorAll(".card");
 
+let firstPick = null;
+let secondPick = null;
+
+// Event handlers
+
 cards.forEach(card => {
   card.addEventListener("click", e => {
-    const card = e.target.parentElement;
-    if (card.classList.contains("rotate-js")) {
-      card.classList.remove("rotate-js");
-    } else {
-      card.classList.add("rotate-js");
+    // Need to check if user actually picked the front of the card
+    if (e.target.classList.contains("card-front")) {
+      // The user will only be allowed to flip a card only if he or she has not picked a first or second card
+      if (!firstPick || !secondPick) {
+        //  Obtain a reference to the whole card... Need to use parentElement since only clicking front
+        const card = e.target.parentElement;
+        //  Obtain a refrence the the back of the card (2nd child div)
+        const cardBack = card.children[1];
+
+        card.classList.add("rotate-js");
+
+        if (!firstPick) {
+          firstPick = cardBack;
+          // Need to check if user selected a card twice. Game will get buggy if do not check for this clause...
+        } else if (cardBack !== firstPick) {
+          secondPick = cardBack;
+        }
+
+        // Only compare the two cards unless they have a value
+        if (firstPick && secondPick) compareCards(firstPick, secondPick);
+      }
     }
   });
 });
@@ -15,15 +38,14 @@ cards.forEach(card => {
 window.addEventListener("load", init);
 
 function init() {
-  const deck = [];
-  cards.forEach(card => deck.push(card));
+  // Assign cards to deck
+  const deck = [...cards];
   // Remove each card from game board
   while (container.firstChild) {
     container.firstChild.remove();
   }
   // Shuffle deck of cards using Fisher Yates Algorithm
   shuffle(deck);
-
   // Reassign shuffled card to the game board
   deck.forEach(card => container.appendChild(card));
 }
@@ -37,4 +59,16 @@ function shuffle(a) {
     [a[i], a[j]] = [a[j], a[i]];
   }
   return a;
+}
+
+function compareCards(firstCard, secondCard) {
+  const result = firstCard.textContent === secondCard.textContent;
+  setTimeout(() => {
+    if (!result) {
+      firstCard.parentElement.classList.remove("rotate-js");
+      secondCard.parentElement.classList.remove("rotate-js");
+    }
+    firstPick = null;
+    secondPick = null;
+  }, 2000);
 }
