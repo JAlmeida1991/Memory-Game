83f94ed1fcd6ae75900080e3ef0b739e5ec5bc53
Jonathan <jalmeida0291@gmail.com>
2018-10-02 20:35:15 -0400

    using font awesome for card back and card front

--- app.js
+++ app.js
@@ -1,107 +1,112 @@
-// Global variables
-const container = document.querySelector(".container");
-const cards = document.querySelectorAll(".card");
-const resetBtn = document.querySelector(".btn");
+(function() {
+  "use strict";
 
-let firstPick;
-let secondPick;
+  // Global variables
+  const container = document.querySelector(".container");
+  const cards = document.querySelectorAll(".card");
+  const resetBtn = document.querySelector(".btn");
 
-// Event handlers
+  let firstPick;
+  let secondPick;
 
-cards.forEach(card => {
-  card.addEventListener("click", e => {
-    // Need to check if user actually picked the front of the card
-    if (e.target.classList.contains("card-front")) {
-      // The user will only be allowed to flip a card only if he or she has not picked a first or second card
-      if (!firstPick || !secondPick) {
-        //  Obtain a reference to the whole card... Need to use parentElement since only clicking front of card
-        const card = e.target.parentElement;
-        //  Obtain a refrence the the back of the card (2nd child div)
-        const cardBack = card.children[1];
-        // Add rotate class to card
-        card.classList.add("rotate-js");
-        // This will assign the values to firstPick and secondPick
-        selectCard(cardBack);
-        // Only compare the two cards unless they have a value
-        if (firstPick && secondPick) compareCards(firstPick, secondPick);
+  // Event handlers
 
-        displayBtn();
+  cards.forEach(card => {
+    card.addEventListener("click", e => {
+      // Need to check if user actually picked the front of the card
+      if (e.target.classList.contains("card-front")) {
+        // The user will only be allowed to flip a card only if he or she has not picked a first or second card
+        if (!firstPick || !secondPick) {
+          //  Obtain a reference to the whole card... Need to use parentElement since only clicking front of card
+          const card = e.target.parentElement;
+          //  Obtain a refrence the the back of the card (2nd child div)
+          const cardBack = card.children[1];
+          // Add rotate class to card
+          card.classList.add("rotate-js");
+          // This will assign the values to firstPick and secondPick
+          selectCard(cardBack);
+          // Only compare the two cards unless they have a value
+          if (firstPick && secondPick) compareCards(firstPick, secondPick);
+
+          displayBtn();
+        }
       }
-    }
+    });
   });
-});
 
-resetBtn.addEventListener("click", () => {
-  // Need to remove any pre-existing rotate-js class from cards
-  cards.forEach(card => card.classList.remove("rotate-js"));
-  // Need a setTimeout call otherwise change will happen instant
-  setTimeout(init, 1000);
-});
+  resetBtn.addEventListener("click", () => {
+    // Need to remove any pre-existing rotate-js class from cards
+    cards.forEach(card => card.classList.remove("rotate-js"));
+    // Need a setTimeout call otherwise change will happen instant
+    setTimeout(init, 1000);
+  });
 
-window.addEventListener("load", init);
+  window.addEventListener("load", init);
 
-/************************
- * Fisher Yates Algorithm:
- * **********************
- */
+  /************************
+   * Fisher Yates Algorithm:
+   * **********************
+   */
 
-function shuffle(a) {
-  for (let i = a.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [a[i], a[j]] = [a[j], a[i]];
-  }
-  return a;
-}
-
-//**********************
-
-function compareCards(firstCard, secondCard) {
-  const result = firstCard.textContent === secondCard.textContent;
-  // Need to use setTimeout since incorrect change will happen instant otherwise
-  setTimeout(() => {
-    if (!result) {
-      firstCard.parentElement.classList.remove("rotate-js");
-      secondCard.parentElement.classList.remove("rotate-js");
+  function shuffle(a) {
+    for (let i = a.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [a[i], a[j]] = [a[j], a[i]];
     }
+    return a;
+  }
+
+  //**********************
+
+  function compareCards(firstCard, secondCard) {
+    const result =
+      firstCard.firstChild.className === secondCard.firstChild.className;
+    // Need to use setTimeout since incorrect change will happen instant otherwise
+    setTimeout(() => {
+      if (!result) {
+        firstCard.parentElement.classList.remove("rotate-js");
+        secondCard.parentElement.classList.remove("rotate-js");
+      }
+      firstPick = null;
+      secondPick = null;
+    }, 2000);
+  }
+
+  function selectCard(cardSide) {
+    // Check if user did not select a card... If he or she has not, assign value as first pick
+    if (!firstPick) {
+      firstPick = cardSide;
+      // Need to check if user selected a card twice. Game will get buggy if do not check for this clause...
+    } else if (cardSide !== firstPick) {
+      secondPick = cardSide;
+    }
+  }
+
+  function init() {
+    // If reset button is showing, hide it
+    resetBtn.style.display = "none";
+
+    // Initalize both firstPick and secondPick as null
     firstPick = null;
     secondPick = null;
-  }, 2000);
-}
-
-function selectCard(cardSide) {
-  // Check if user did not select a card... If he or she has not, assign value as first pick
-  if (!firstPick) {
-    firstPick = cardSide;
-    // Need to check if user selected a card twice. Game will get buggy if do not check for this clause...
-  } else if (cardSide !== firstPick) {
-    secondPick = cardSide;
+    // Assign cards to deck
+    const deck = [...cards];
+    // Remove each card from game board
+    while (container.firstChild) {
+      container.firstChild.remove();
+    }
+    // Shuffle deck of cards using Fisher Yates Algorithm
+    shuffle(deck);
+    // Reassign shuffled card to the game board
+    deck.forEach(card => container.appendChild(card));
   }
-}
 
-function init() {
-  // If reset button is showing, hide it
-  resetBtn.style.display = "none";
-
-  // Initalize both firstPick and secondPick as null
-  firstPick = null;
-  secondPick = null;
-  // Assign cards to deck
-  const deck = [...cards];
-  // Remove each card from game board
-  while (container.firstChild) {
-    container.firstChild.remove();
+  function displayBtn() {
+    const result = Array.from(cards).every(card =>
+      card.classList.contains("rotate-js")
+    );
+    if (result) {
+      resetBtn.style.display = "block";
+    }
   }
-  // Shuffle deck of cards using Fisher Yates Algorithm
-  shuffle(deck);
-  // Reassign shuffled card to the game board
-  deck.forEach(card => container.appendChild(card));
-}
-
-function displayBtn() {
-  const result = Array.from(cards).every(card =>
-    card.classList.contains("rotate-js")
-  );
-  if (result) {
-    resetBtn.style.display = "block";
-  }
-}
+})();
